cb3abeac0fd0cee30d614cf23e11c5867d546ada
Farid <faridfaracli9@gmail.com>
2024-05-18 18:16:54 +0400

    Nvim-tree's root directory changes with cwd and toggle-term is now of better size

--- nvim/lua/toggle_term.lua
+++ nvim/lua/toggle_term.lua
@@ -2,7 +2,7 @@ local Setup = {
 	terminal_mappings = true                       ,
 	insert_mappings   = false                      ,
 	open_mapping      = '<c-t>'                    ,
-	float_opts        = {height = 40, width = 160} ,
+	float_opts        = {height = 42, width = 160} ,
 	direction         = 'float'                    ,
 	autochdir         = true                       ,
 }
